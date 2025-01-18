1e3490b5218ade9594b86bf9a0c34a941ce9ba3e
Edmilson Monteiro Rodrigues Neto <edmilson.rodriguesn38@gmail.com>
2025-01-18 14:53:48 +0000

    feat: created the contact component

--- nuteral-frontend/src/app/pages/contact/contact.component.ts
+++ nuteral-frontend/src/app/pages/contact/contact.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+      // Here you would typically send the form data to your backend
+      this.contactForm.reset();
+    }
+  }
+}
